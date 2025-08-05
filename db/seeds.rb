4b07b6516415d7d4cc7291d8d75331bc47d46fad
Chiba <chi.11220214.yo@gmail.com>
2025-08-06 02:08:10 +0900

    fackerよるダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-Article.create({ title: "テスト記事", content: "accumsan orci non nunc varius placerat. Quisque et dolor venenatis, pretium mi at, vehicula libero. Vestibulum vel ultricies odio. Cras" } )
-Article.create({ title: "ダミー記事", content: "Pellentesque sed lectus non augue vulputate elementum. Sed tempor malesuada nisl id interdum. Nam malesuada eget massa sed ullamcorper." } )
+10.times do
+Article.create(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
