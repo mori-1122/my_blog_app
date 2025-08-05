2c5be4408198aac65c03b2bc434bed4481e6538e
Chiba <chi.11220214.yo@gmail.com>
2025-08-06 02:51:50 +0900

    lint修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 10.times do
-Article.create(
-    title: Faker::Lorem.sentence(word_count: 5),
-    content: Faker::Lorem.sentence(word_count: 100)
-  )
+  Article.create(
+      title: Faker::Lorem.sentence(word_count: 5),
+      content: Faker::Lorem.sentence(word_count: 100)
+    )
 end
