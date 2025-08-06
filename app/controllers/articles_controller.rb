f1cd1b21af2f1f9216f38e89540a9bbc8536fcc6
Chiba <chi.11220214.yo@gmail.com>
2025-08-06 12:03:27 +0900

    createアクションを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.all # 複数ある投稿記事の一覧を表示する
   end
 
   def show
@@ -9,5 +9,20 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params) # 保存する箱みたいなものを作成
+    if @article.save # 保存する
+      redirect_to article_path(@article) # リダイレクトされる
+    else
+      render :new, status: :unprocessable_entity# 保存されなかったらnewに行く
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content) # articleっていうキーを持っているか求める タイトル、コンテンツを抜き出す
   end
 end
