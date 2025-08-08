8191fcbed59ea1344bfcaf6acee09157da096fb0
Chiba <chi.11220214.yo@gmail.com>
2025-08-09 02:35:54 +0900

    記事を削除できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "articles#index" # rootは/
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :articles, only: [ :show, :new, :create, :edit, :update ]
+  resources :articles, only: [ :show, :new, :create, :edit, :update, :destroy ]
 end
